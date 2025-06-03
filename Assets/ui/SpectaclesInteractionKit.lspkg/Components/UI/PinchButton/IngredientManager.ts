3978e97af76eaa9c5a4b2a725c2914377c83df5f
Andy <andyyao15@gmail.com>
2025-06-02 22:44:04 -0400

    functional but uglyyyy ui

--- Assets/ui/SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/IngredientManager.ts
+++ Assets/ui/SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/IngredientManager.ts
@@ -1,53 +1,48 @@
-import {InteractorEvent} from "../../../Core/Interactor/InteractorEvent"
-import Event from "../../../Utils/Event"
-import {createCallback} from "../../../Utils/InspectorCallbacks"
-import NativeLogger from "../../../Utils/NativeLogger"
-import {Interactable} from "../../Interaction/Interactable/Interactable"
-
-
 @component
 export class IngredientManager extends BaseScriptComponent {
   @input
-  textObject: SceneObject; // The slide or text object
+  ingredientObjects: SceneObject[]; // Ingredient scene objects (must have Text components)
 
   @input
-  buttonObject: SceneObject; // The pinch button
+  ingredientNumbers: number[]; // Parallel array of numbers to display
 
-  IngredientCount: number = 1;
-
-  private textComponent: Text | null = null;
-  private buttonInteractable: Interactable | null = null;
+  private textComponents: Text[] = [];
 
   onAwake(): void {
-    // Get and validate the text component
-    this.textComponent = this.textObject?.getComponent("Component.Text");
-    if (!this.textComponent) {
-      print("Error: textObject has no Text component.");
-      return;
-    }
-
-    // Get and validate the interactable
-    this.buttonInteractable = this.buttonObject?.getComponent(Interactable.getTypeName());
-    if (!this.buttonInteractable) {
-      print("Error: buttonObject is missing an Interactable component.");
-      return;
-    }
-
-    // Initial update
-    this.updateText();
-
-    // Set up trigger event
-    this.createEvent("OnStartEvent").bind(() => {
-      this.buttonInteractable?.onTriggerEnd.add((event: InteractorEvent) => {
-        if (this.enabled) {
-          this.IngredientCount++;
-          this.updateText();
-        }
-      });
+    // Cache Text components
+    this.textComponents = this.ingredientObjects.map((obj, index) => {
+      const text = obj.getComponent("Component.Text") as Text;
+      if (!text) {
+        print(`Warning: ingredientObjects[${index}] has no Text component`);
+      }
+      return text;
     });
+
+    this.updateAllTexts();
   }
 
-  private updateText(): void {
-    this.textComponent!.text = this.IngredientCount.toString();
+  updateAllTexts(): void {
+    for (let i = 0; i < this.textComponents.length; i++) {
+      const textComp = this.textComponents[i];
+      const value = this.ingredientNumbers[i];
+
+      if (textComp) {
+        textComp.text = value.toString();
+      }
+    }
+  }
+
+  setIngredientNumber(index: number, value: number): void {
+    if (index < 0 || index >= this.ingredientNumbers.length) {
+      print("Invalid ingredient index.");
+      return;
+    }
+
+    this.ingredientNumbers[index] = value;
+
+    const textComp = this.textComponents[index];
+    if (textComp) {
+      textComp.text = value.toString();
+    }
   }
 }
