1ac809c041915f2fb32faed74c8a9918f2fd194d
Andy <andyyao15@gmail.com>
2025-06-03 23:06:46 -0400

    quest activation

--- Assets/ui/SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/MenuManager.ts
+++ Assets/ui/SpectaclesInteractionKit.lspkg/Components/UI/PinchButton/MenuManager.ts
@@ -4,7 +4,7 @@ import { Interactable } from "../../Interaction/Interactable/Interactable";
 @component
 export class MenuManager extends BaseScriptComponent {
   @input
-  questButtons: SceneObject[]; // Buttons that trigger quest menus
+  questButtons: SceneObject[]; // Parents of buttons that trigger quest menus
 
   @input
   questMenus: SceneObject[]; // Corresponding quest menu objects
@@ -18,7 +18,7 @@ export class MenuManager extends BaseScriptComponent {
   potionButton: SceneObject;
   @input
   potionMenu: SceneObject;
-    
+
   @input
   questButton: SceneObject;
   @input
@@ -26,32 +26,63 @@ export class MenuManager extends BaseScriptComponent {
 
   @input idleButtonMaterials: Material[];
   @input activeButtonMaterials: Material[];
+    
+  public activeQuest = -1;
 
   onAwake(): void {
-    // Attach quest button listeners
-    this.questButtons.forEach((buttonObj, index) => {
-      const interactable = buttonObj.getComponent(Interactable.getTypeName());
-      if (!interactable) {
-        print(`Quest button ${index} missing Interactable.`);
-        return;
-      }
+    this.createEvent("OnStartEvent").bind(() => {
+  // Setup listeners for quest menu buttons (inside menus)
+  this.questMenus.forEach((menuObj, index) => {
+    const childButton = menuObj.children.find(child =>
+      child.getComponent(Interactable.getTypeName())
+    );
+    if (!childButton) {
+      print(`No Interactable child found in questMenu[${index}]`);
+      return;
+    }
 
-      this.createEvent("OnStartEvent").bind(() => {
+    const interactable = childButton.getComponent(Interactable.getTypeName());
+    if (!interactable) {
+      print(`Child in questMenu[${index}] has no Interactable`);
+      return;
+    }
+
+    interactable.onTriggerEnd.add((event: InteractorEvent) => {
+      if (this.enabled) {
+        this.setQuestActive(index);
+      }
+    });
+  });
+
+    this.questButtons.forEach((parentObj, index) => {
+        const buttonObj = parentObj.children.find(child =>
+          child.getComponent(Interactable.getTypeName())
+        );
+        if (!buttonObj) {
+          print(`Child button missing for questButton parent at index ${index}`);
+          return;
+        }
+    
+        const interactable = buttonObj.getComponent(Interactable.getTypeName());
+        if (!interactable) {
+          print(`Interactable missing on child at index ${index}`);
+          return;
+        }
+    
         interactable.onTriggerEnd.add((event: InteractorEvent) => {
           if (this.enabled) {
             this.showOnlyMenu(this.questMenus[index]);
           }
         });
       });
-    });
-
+});
     // Ingredient button
     const ingredientInteractable = this.ingredientButton.getComponent(Interactable.getTypeName());
     if (ingredientInteractable) {
       this.createEvent("OnStartEvent").bind(() => {
         ingredientInteractable.onTriggerEnd.add((event: InteractorEvent) => {
           if (this.enabled) {
-            this.setIngredientMenuActive()
+            this.setIngredientMenuActive();
           }
         });
       });
@@ -63,12 +94,12 @@ export class MenuManager extends BaseScriptComponent {
       this.createEvent("OnStartEvent").bind(() => {
         potionInteractable.onTriggerEnd.add((event: InteractorEvent) => {
           if (this.enabled) {
-            this.setPosionMenuActive()
+            this.setPotionMenuActive();
           }
         });
       });
     }
-        
+
     // Quest menu button
     const questInteractable = this.questButton.getComponent(Interactable.getTypeName());
     if (questInteractable) {
@@ -85,6 +116,23 @@ export class MenuManager extends BaseScriptComponent {
     this.setQuestMenuActive();
   }
 
+  private setQuestActive(n: number): void {
+    this.setQuestMenuActive();
+    this.activeQuest = n;
+
+    // Loop through questButtons and disable all child buttons
+    this.questButtons.forEach((parent, index) => {
+      const childButton = parent.children.find(child => child.getComponent(Interactable.getTypeName()));
+      if (childButton) {
+        childButton.enabled = index === n; // Enable only the selected one
+      }
+      const textComp = parent.getComponent("Component.Text");
+      if (textComp) {
+        textComp.textFill.color = index === n ? new vec4(0, 1, 0, 1) : new vec4(1, 1, 1, 1);
+      }
+    });
+  }
+
   private setQuestMenuActive(): void {
     this.showOnlyMenu(this.questMenu);
     this.setButtonMaterial(this.ingredientButton, this.idleButtonMaterials);
@@ -99,7 +147,7 @@ export class MenuManager extends BaseScriptComponent {
     this.setButtonMaterial(this.potionButton, this.idleButtonMaterials);
   }
 
-  private setPosionMenuActive(): void {
+  private setPotionMenuActive(): void {
     this.showOnlyMenu(this.potionMenu);
     this.setButtonMaterial(this.ingredientButton, this.idleButtonMaterials);
     this.setButtonMaterial(this.questButton, this.idleButtonMaterials);
@@ -116,14 +164,18 @@ export class MenuManager extends BaseScriptComponent {
   }
 
   private setButtonMaterial(button: SceneObject, materials: Material[]): void {
-    const buttonMesh = button.children.find((child) => child.name === 'Button Mesh')
+    const buttonMesh = button.children.find(child => child.name === 'Button Mesh');
     if (!buttonMesh) {
-        print(`${button.name} doesn't have Button Mesh`)
+      print(`${button.name} doesn't have Button Mesh`);
+      return;
     }
-    const renderMeshVisual = buttonMesh.getComponent('RenderMeshVisual')
+
+    const renderMeshVisual = buttonMesh.getComponent('RenderMeshVisual');
     if (!renderMeshVisual) {
-        print(`${button.name} Button Mesh doesn't have renderMeshVisual`)
+      print(`${button.name} Button Mesh doesn't have renderMeshVisual`);
+      return;
     }
-    renderMeshVisual.materials = materials
+
+    renderMeshVisual.materials = materials;
   }
 }
